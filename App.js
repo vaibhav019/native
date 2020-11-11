d56536d4fdb468905e58671ee8f174b0bb76146f
vaibhav019 <vaibhavsengarnetid@gmail.com>
2020-11-11 21:36:52 +0530

    initial setup

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,SafeAreaView } from 'react-native';
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    
+    <SafeAreaView style={styles.container}>
+      <Text> this is vaibhav singh sengar </Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'dodgerblue',
+    textDecorationColor:'danger'
+    /*alignItems: 'center',
+    justifyContent: 'center',*/
   },
 });
+
